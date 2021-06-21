bb845ce5f09173881afbcc6f4835b36fe34be475
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2021-06-21 13:48:57 +0530

    fuction for matching two cards

--- Day-10/Day-10.js
+++ Day-10/Day-10.js
@@ -1,3 +1,8 @@
 const cards = document.querySelectorAll('.Card');
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+function match() {
+  let isMatch = fst.dataset.name === snd.dataset.name;
+
+  isMatch ? disableCards() : unflipCards();
+}
