4b9f16e8952410366fa4909ebaa3005cacb9f77a
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2021-06-24 14:16:06 +0530

    basic function for each question

--- Day-11/Day-11.js
+++ Day-11/Day-11.js
@@ -1,1 +1,8 @@
-
+function basicFunction(ques,a,b,c,d,correct) {
+    this.ques= ques,
+    this.optiona=a,
+    this.optionb=b,
+    this.optionc=c,
+    this.optiond=d,
+    this.correct=correct;
+  }
