3a769b982a90747acede4765602517c6dfcb6712
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2021-07-03 11:14:40 +0530

    Day-16 part2

---
+++ Day-16/day-16-2/src/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+<div className="main">
+    <h1>MY CHESS BOARD♟</h1>
+<div className="division">
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    </div>
+    <div className="rows">
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+    <div className="block"></div>
+   </div>
+</div>
+</div>,
+     document.getElementById("root")
+);
