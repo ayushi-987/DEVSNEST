0dc793f29d6f301ff8419fd857852c720cfc3bab
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2021-07-11 16:11:44 +0530

    Day-18 THA

---
+++ day-18/src/box2.jsx
@@ -0,0 +1,17 @@
+
+const styleforBox2 ={
+    backgroundColor: "white",
+    width: "12.5%",
+    height: "12.5%",
+    display: "inline-block",
+    float: "left",
+}
+
+const Box2 = () =>{
+    return (
+     <div style={styleforBox2}>
+     </div>
+    );
+};
+
+export default Box2;
